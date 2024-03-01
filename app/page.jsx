46e8ef96f0c1e626a24181d6d690dd79f977613b
Deepika Singh <deepikaS050410@gmail.com>
2024-03-01 15:36:50 +0530

    controlled overflow in  Weather and Page.jsx files

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 function Page() {
   return (
-    <div className="overflow-x-hidden overflow-y-scroll h-[100vh]">
-      <div className="absolute top-0 right-0 bottom-0 left-0 bg-black/75 z-[1] h-full w-screen" />
+    <div className="overflow-x-hidden overflow-y-scroll h-[100vh] ">
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-black/75 z-[1] h-[100vh] w-[100vw]" />
 
       <Image
         src="/assets/images/weatherIma.jpg"
